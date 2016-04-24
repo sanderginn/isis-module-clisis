9344b277a821c7efee0b63e355c6553b6112baa1
Sander Ginn <sanderginn@users.noreply.github.com>
2016-04-24 14:51:40 +0200

    CLI-1: attempting authentication based on ContactApp

--- webapp/src/main/webapp/cli/js/app.js
+++ webapp/src/main/webapp/cli/js/app.js
@@ -1,4 +1,9 @@
-var app = angular.module("CLIsis", ['ngRoute']);
+var app = angular.module("clisis", ['ngRoute', 'clisis.services.preferences', 'clisis.services.authentication']);
+
+app.value('AppConfig', {
+  appPrefix: 'clisis',
+  baseUrl: "http://127.0.0.1:8080"   // TODO: move to preferences service ?
+});
 
 app.config(function($routeProvider) {
   $routeProvider
